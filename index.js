1970e414d9cd1dcf2e8ca7f23e1f5897186ad54f
Seda Ünal <sedaylmz06@outlook.com>
2022-12-17 00:10:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
